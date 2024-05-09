ba469dc9cd18283f3b97134274dc8b709cd1454a
levanovroman <levanovroman2016@yandex.ru>
2024-05-09 16:56:42 +0300

    intellij

--- src/java/main/Test.java
+++ src/java/main/Test.java
@@ -3,5 +3,11 @@ package java.main;
 public class Test {
     public static void main(String[] args) {
 
+        class NewTestFile{
+            int a;
+            int b;
+            int c;
+
+        }
     }
 }
